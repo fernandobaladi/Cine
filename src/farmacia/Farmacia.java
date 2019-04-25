09b4ee4a2d851158f319d682d0168d3d71194805
FernandoBaladi <fernandobaladit@gmail.com>
2019-04-25 15:43:56 -0400

    segundo commit

--- src/farmacia/Farmacia.java
+++ src/farmacia/Farmacia.java
@@ -18,6 +18,7 @@ public class Farmacia {
         medQuimico atamel = new medQuimico(0001, 1000, "Atamel", 3000);
         System.out.println("El precio del atamel es: "+atamel.getPrecio());
         System.out.println("El componente es:"+atamel.getComponente());
+        System.out.println("Estoy vivo");
         
         Clientes c1 = new Clientes("Antonio",  "Guerra", "M", "San Antonio",  24464173, 989733);
         c1.Comprar();
